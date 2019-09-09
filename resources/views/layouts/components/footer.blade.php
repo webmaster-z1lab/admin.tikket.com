a0811103c80156e43111c9e3c774aba327f7afa2
Olimar Ferraz <webmaster@z1lab.com.br>
2019-09-09 19:37:01 -0300

    Added help text to event forms;

--- resources/views/layouts/components/footer.blade.php
+++ resources/views/layouts/components/footer.blade.php
@@ -7,6 +7,7 @@
 
             <div class="col-md-6">
                 <div class="text-md-right footer-links d-none d-md-block">
+                    <a href="{{ config('app.domains.main_site') }}">Site principal</a>
                     <a href="{{ config('app.domains.main_site') }}/contato-e-suporte">Suporte</a>
                     <a href="{{ config('app.domains.main_site') }}/termos-de-uso">Termos de uso</a>
                     <a href="{{ config('app.domains.main_site') }}/privacidade-cookies">Privacidade</a>
